2ec105aff3a56dca21d302c0a99505647acccdd8
kalharatennakoon <kalharatennakoonmck@gmail.com>
2024-03-25 19:30:02 +0530

    connect mongodb database

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hello World!");
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// app.listen(3000, () => {
+//     console.log("Server is listening on port 3000");
+// });
+
+app.get("/", (req, res) => {
+    res.send("Hello from Node Server");
+});
+
+mongoose.connect("") // mongodb connection URL
+.then(() =>{
+    console.log("Connected to database!");
+    app.listen(3000, () => {
+        console.log("Server is listening on port 3000");
+    });
+})
+.catch(() => {
+    console.log("Connection failed!");
+});
