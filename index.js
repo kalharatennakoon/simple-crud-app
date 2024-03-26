491338388a889739ec50642f1c639f8eeb7b2c0d
kalharatennakoon <kalharatennakoonmck@gmail.com>
2024-03-26 10:26:05 +0530

    add routes, controllers

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Product = require('./models/product.model.js');
+const productRoute = require("./routes/product.route.js");
 
 const app = express();
 
+
+// middleware configuration
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+
+// routes
+app.use("/api/products", productRoute);
+
 
 // app.listen(3000, () => {
 //     console.log("Server is listening on port 3000");
@@ -15,42 +24,86 @@ app.get("/", (req, res) => {
 });
 
 
-app.get("/api/products", async (req, res) => {
-    try{
-        const products = await Product.find({});
-        res.status(200).json(products);
+// app.get("/api/products", async (req, res) => {
+//     try{
+//         const products = await Product.find({});
+//         res.status(200).json(products);
 
-    }catch{
-        res.status(500).json({message: error.message});
-    }
-});
-
-
-app.get("/api/products/:id", async (req, res) => {
-    try{
-        const { id } = req.params;
-        const product = await Product.findById(id);
-        res.status(200).json(product);
-
-    }catch{
-        res.status(500).json({message: error.message});
-    }
-});
+//     }catch(error){
+//         res.status(500).json({message: error.message});
+//     }
+// });
 
 
-app.post("/api/products", async (req, res) => {
-    //res.send("Data Received.");
-    // console.log(req.body);
-    // res.send(req.body);
-    try{
-        const product = await Product.create(req.body);
-        res.status(200).json(product);
+// app.get("/api/products/:id", async (req, res) => {
+//     try{
+//         const { id } = req.params;
+//         const product = await Product.findById(id);
+//         res.status(200).json(product);
 
-    }catch{
-        res.status(500).json({message: error.message});
-    }
+//     }catch(error){
+//         res.status(500).json({message: error.message});
+//     }
+// });
 
-});
+
+// app.post("/api/products", async (req, res) => {
+//     //res.send("Data Received.");
+//     // console.log(req.body);
+//     // res.send(req.body);
+
+//     try{
+//         const product = await Product.create(req.body);
+//         res.status(200).json(product);
+
+//     }catch(error){
+//         res.status(500).json({message: error.message});
+//     }
+
+// });
+
+
+// update a product
+// app.put("/api/products/:id", async(req, res) => {
+//     try{
+
+//         const {id} = req.params;
+
+//         const product = await Product.findByIdAndUpdate(id, req.body);
+
+//         if(!product){
+//             res.status(404).json({message: "Product not found"});
+//         }
+
+//         const updatedProduct = await Product.findById(id);
+//         res.status(200).json(updatedProduct);
+
+//     }catch(error){
+//         res.status(500).json({message: error.message});
+//     }
+// });
+
+
+// delete a product
+// app.delete("/api/products/:id", async (req, res) => {
+//     try{
+//         const {id} = req.params;
+
+//         const product = await Product.findByIdAndDelete(id);
+
+//         if(!product){
+//             res.status(404).json({message: "Product not found"});
+//         }
+
+//         res.status(200).json({message: "Product deleted successfully"});
+
+//     }catch(error){
+//         res.status(500).json({message: error.message});
+//     }
+// });
+
+
+
 
 
 mongoose.connect("") // mongodb connection URL
